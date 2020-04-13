3fd1c9a5c1740eae354d65f47c6b1e87ff6104fe
Daniel Keyes <danieljkeyes@gmail.com>
2020-04-13 01:12:16 -0500

    wrap NvCloth Factory, Fabric, and Cloth functions

--- python/pynvcloth_sanity_check.py
+++ python/pynvcloth_sanity_check.py
@@ -4,17 +4,17 @@ import os
 # You may need to supply a value for PYTHONPATH if this is not your build 
 # directory
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../build/bin/'))
-import pynvcloth
+import pynvcloth as nvc
 
 # The NvCloth environment must be setup (and destroyed) explicitly.
 # daniel: This doesn't seem very python-like. Can we do this implicitly?
-pynvcloth.allocate_env()
+nvc.allocate_env()
 
 # Cloth factories can be created via the appropriate constructor.
-null_factory = pynvcloth.Factory()
+null_factory = nvc.Factory()
 print(type(null_factory))
 
-factory = pynvcloth.create_factory_cpu()
+factory = nvc.create_factory_cpu()
 print(type(factory))
 
 # Note: resources need to be explicitly freed. We could also control this with function scoping
@@ -23,9 +23,41 @@ del null_factory
 del factory
 
 def scoped_alloc():
-    another = pynvcloth.create_factory_cpu()
+    another = nvc.create_factory_cpu()
     print(type(another))
 
 scoped_alloc()
 
-pynvcloth.free_env()
+cmd = nvc.ClothMeshDesc()
+cmd.set_to_default()
+print(cmd.is_valid(), flush=True)
+
+# Note: these must be declared as named variables so they stay in scope while the mesh is created.
+triangles = nvc.VectorTri([nvc.Triangle(0, 1, 3), nvc.Triangle(0, 2, 3), nvc.Triangle(2, 3, 5), nvc.Triangle(2, 4, 5)])
+points = nvc.VectorNx3([
+    nvc.Vec3(0.0, 0.0, 0.0),
+    nvc.Vec3(0.0, 1.0, 0.0),
+    nvc.Vec3(1.0, 0.0, 0.0),
+    nvc.Vec3(1.0, 1.0, 0.0),
+    nvc.Vec3(2.0, 0.0, 0.0),
+    nvc.Vec3(2.0, 1.0, 0.0),
+])
+inv_masses = nvc.VectorFloat([1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
+point_masses = nvc.VectorNx4([nvc.Vec4(v, w) for (v, w) in zip(points, inv_masses)])
+
+cmd.triangles = nvc.as_bounded_data(triangles)
+cmd.points = nvc.as_bounded_data(points)
+cmd.inv_masses = nvc.as_bounded_data(inv_masses)
+
+print(cmd.is_valid(), flush=True)
+
+factory = nvc.create_factory_cpu()
+gravity = nvc.Vec3(0., -9.8, 0.);
+fabric = nvc.cook_fabric_from_mesh(factory, cmd, gravity, False)
+cloth = factory.create_cloth(point_masses, fabric)
+
+del cloth
+del fabric
+del factory
+
+nvc.free_env()
--- src/nvcloth_wrapper.cpp
+++ src/nvcloth_wrapper.cpp
@@ -1,10 +1,22 @@
-#include <pybind11/pybind11.h>
+#include <iostream>
+#include <memory>
+#include <vector>
 
+#include <pybind11/pybind11.h>
+#include <pybind11/stl_bind.h>
+
+#include <NvCloth/Cloth.h>
+#include <NvCloth/Fabric.h>
 #include <NvCloth/Factory.h>
+#include <NvClothExt/ClothFabricCooker.h>
 
 #include "CallbackImplementations.h"
 
 using std::unique_ptr;
+using std::vector;
+
+using nv::cloth::BoundedData;
+using nv::cloth::ClothMeshDesc;
 
 namespace py = pybind11;
 
@@ -16,12 +28,73 @@ struct FactoryDeleter {
 
 using Factory = unique_ptr<nv::cloth::Factory, FactoryDeleter>;
 
+struct FabricDeleter {
+  void operator()(nv::cloth::Fabric* const f) const { f->decRefCount(); }
+};
+
+using Fabric = unique_ptr<nv::cloth::Fabric, FabricDeleter>;
+
+struct ClothDeleter {
+  void operator()(nv::cloth::Cloth* const c) const { NV_CLOTH_DELETE(c); }
+};
+
+using Cloth = unique_ptr<nv::cloth::Cloth, ClothDeleter>;
+
 // We are using unique_ptr to enforce custom deleters, so don't let pybind11 try
 // to extract out the raw pointers.
 PYBIND11_MAKE_OPAQUE(Factory);
+PYBIND11_MAKE_OPAQUE(Fabric);
+PYBIND11_MAKE_OPAQUE(Cloth);
+
+struct Triangle {
+  Triangle() : a(0), b(0), c(0) {}
+
+  Triangle(const uint32_t a, const uint32_t b, const uint32_t c)
+      : a(a), b(b), c(c) {}
+
+  uint32_t a, b, c;
+};
+
+struct Quad {
+  Quad() : a(0), b(0), c(0), d(0) {}
+
+  Quad(const uint32_t a, const uint32_t b, const uint32_t c, const uint32_t d)
+      : a(a), b(b), c(c), d(d) {}
+
+  uint32_t a, b, c, d;
+};
 
 auto create_factory_cpu() {
   return Factory(NvClothCreateFactoryCPU());
+}
+
+// We need to be able to pass in arrays with C++-managed memory (since
+// BoundedData requires a long-lived pointer to contiguous memory).
+// Therefore, create opaque types for vector, and use bind_vector later to
+// emulate vector functions in python
+PYBIND11_MAKE_OPAQUE(vector<Triangle>);
+PYBIND11_MAKE_OPAQUE(vector<Quad>);
+PYBIND11_MAKE_OPAQUE(vector<float>);
+PYBIND11_MAKE_OPAQUE(vector<int32_t>);
+PYBIND11_MAKE_OPAQUE(vector<physx::PxVec3>);
+PYBIND11_MAKE_OPAQUE(vector<physx::PxVec4>);
+
+template <typename T>
+auto bounded_data_view(const vector<T>& vec) {
+  BoundedData result;
+  result.count = static_cast<physx::PxU32>(vec.size());
+  result.stride = sizeof(T);
+  result.data = vec.data();
+  return result;
+}
+
+Fabric cook_fabric_from_mesh(const Factory& factory,
+                             const ClothMeshDesc& desc,
+                             const physx::PxVec3& gravity,
+                             const bool use_geodesic_tether) {
+  // TODO(daniel): is phaseTypes important? currently just throwing it away.
+  return Fabric(NvClothCookFabricFromMesh(factory.get(), desc, gravity, nullptr,
+                                          use_geodesic_tether));
 }
 
 PYBIND11_MODULE(pynvcloth, m) {
@@ -32,7 +105,81 @@ PYBIND11_MODULE(pynvcloth, m) {
   m.def("free_env", &NvClothEnvironment::FreeEnv,
         "De-initialize the NvCloth library.");
 
-  py::class_<Factory>(m, "Factory").def(py::init<>());
+  py::class_<Factory>(m, "Factory")
+      .def(py::init<>())
+      .def("create_cloth",
+           [](Factory& factory, vector<physx::PxVec4>& particle_positions,
+              Fabric& fabric) {
+             return Cloth(factory->createCloth(
+                 nv::cloth::Range<physx::PxVec4>(
+                     &particle_positions[0],
+                     &particle_positions[0] + particle_positions.size()),
+                 *fabric));
+           });
 
   m.def("create_factory_cpu", &create_factory_cpu);
+
+  py::class_<Fabric>(m, "Fabric").def(py::init<>());
+  py::class_<Cloth>(m, "Cloth").def(py::init<>());
+
+  py::class_<Triangle>(m, "Triangle")
+      .def(py::init<>())
+      .def(py::init<uint32_t, uint32_t, uint32_t>())
+      .def_readwrite("a", &Triangle::a)
+      .def_readwrite("b", &Triangle::b)
+      .def_readwrite("c", &Triangle::c);
+
+  py::class_<Quad>(m, "Quad")
+      .def(py::init<>())
+      .def(py::init<uint32_t, uint32_t, uint32_t, uint32_t>())
+      .def_readwrite("a", &Quad::a)
+      .def_readwrite("b", &Quad::b)
+      .def_readwrite("c", &Quad::c)
+      .def_readwrite("c", &Quad::c);
+
+  py::class_<physx::PxVec3>(m, "Vec3")
+      .def(py::init<>())
+      .def(py::init<float, float, float>())
+      .def_readwrite("x", &physx::PxVec3::x)
+      .def_readwrite("y", &physx::PxVec3::y)
+      .def_readwrite("z", &physx::PxVec3::z);
+
+  py::class_<physx::PxVec4>(m, "Vec4")
+      .def(py::init<>())
+      .def(py::init<float, float, float, float>())
+      .def(py::init<physx::PxVec3, float>())
+      .def_readwrite("x", &physx::PxVec4::x)
+      .def_readwrite("y", &physx::PxVec4::y)
+      .def_readwrite("z", &physx::PxVec4::z)
+      .def_readwrite("w", &physx::PxVec4::w);
+
+  py::class_<BoundedData>(m, "BoundedData").def(py::init<>());
+
+  // (void) to suppress warning C26444 on MSVC
+  (void)py::bind_vector<vector<Triangle>>(m, "VectorTri");
+  (void)py::bind_vector<vector<Quad>>(m, "VectorQuad");
+  (void)py::bind_vector<vector<float>>(m, "VectorFloat");
+  (void)py::bind_vector<vector<int32_t>>(m, "VectorInt");
+  (void)py::bind_vector<vector<physx::PxVec3>>(m, "VectorNx3");
+  (void)py::bind_vector<vector<physx::PxVec4>>(m, "VectorNx4");
+
+  m.def("as_bounded_data", &bounded_data_view<Triangle>);
+  m.def("as_bounded_data", &bounded_data_view<Quad>);
+  m.def("as_bounded_data", &bounded_data_view<float>);
+  m.def("as_bounded_data", &bounded_data_view<int32_t>);
+  m.def("as_bounded_data", &bounded_data_view<physx::PxVec3>);
+  m.def("as_bounded_data", &bounded_data_view<physx::PxVec4>);
+
+  py::class_<ClothMeshDesc>(m, "ClothMeshDesc")
+      .def(py::init<>())
+      .def_readwrite("points", &ClothMeshDesc::points)
+      .def_readwrite("points_stiffness", &ClothMeshDesc::pointsStiffness)
+      .def_readwrite("inv_masses", &ClothMeshDesc::invMasses)
+      .def_readwrite("triangles", &ClothMeshDesc::triangles)
+      .def_readwrite("quads", &ClothMeshDesc::quads)
+      .def_readwrite("flags", &ClothMeshDesc::flags)
+      .def("set_to_default", &ClothMeshDesc::setToDefault)
+      .def("is_valid", &ClothMeshDesc::isValid);
+
+  m.def("cook_fabric_from_mesh", &cook_fabric_from_mesh);
 }
