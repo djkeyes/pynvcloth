b55d7b52c23abd398d8270366cbbdb0495909044
Daniel Keyes <danieljkeyes@gmail.com>
2020-04-13 03:48:16 -0500

    visualize simulation with trimesh (and enable gravity)

--- python/pynvcloth_sanity_check.py
+++ python/pynvcloth_sanity_check.py
@@ -6,6 +6,11 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../build/bin/'))
 import pynvcloth as nvc  # noqa: E402
 
+import numpy as np
+import trimesh
+from trimesh.viewer import SceneViewer
+import pyglet
+
 # The NvCloth environment must be setup (and destroyed) explicitly.
 # daniel: This doesn't seem very python-like. Can we do this implicitly?
 nvc.allocate_env()
@@ -34,17 +39,26 @@ cmd = nvc.ClothMeshDesc()
 cmd.set_to_default()
 print(cmd.is_valid(), flush=True)
 
+vertices = [
+    [0., 0., 0.],
+    [0., 1., 0.],
+    [1., 0., 0.],
+    [1., 1., 0.],
+    [2., 0., 0.],
+    [2., 1., 0.],
+]
+faces = [
+    [0, 3, 1],
+    [0, 2, 3],
+    [2, 5, 3],
+    [2, 4, 5]
+]
+render_mesh = trimesh.Trimesh(vertices, faces, process=False)
+
 # Note: these must be declared as named variables so they stay in scope while the mesh is created.
-triangles = nvc.VectorTri([nvc.Triangle(0, 1, 3), nvc.Triangle(0, 2, 3), nvc.Triangle(2, 3, 5), nvc.Triangle(2, 4, 5)])
-points = nvc.VectorNx3([
-    nvc.Vec3(0.0, 0.0, 0.0),
-    nvc.Vec3(0.0, 1.0, 0.0),
-    nvc.Vec3(1.0, 0.0, 0.0),
-    nvc.Vec3(1.0, 1.0, 0.0),
-    nvc.Vec3(2.0, 0.0, 0.0),
-    nvc.Vec3(2.0, 1.0, 0.0),
-])
-inv_masses = nvc.VectorFloat([1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
+triangles = nvc.VectorTri([nvc.Triangle(f[0], f[1], f[2]) for f in faces])
+points = nvc.VectorNx3([nvc.Vec3(v[0], v[1], v[2]) for v in vertices])
+inv_masses = nvc.VectorFloat([0.0, 1.0, 1.0, 1.0, 1.0, 1.0])  # anchor particle 0
 point_masses = nvc.VectorNx4([nvc.Vec4(v, w) for (v, w) in zip(points, inv_masses)])
 
 cmd.triangles = nvc.as_bounded_data(triangles)
@@ -61,12 +75,30 @@ solver = factory.create_solver()
 solver.add_cloth(cloth)
 
 cloth.set_solver_frequency(300)
-for _ in range(3):
-    cur_particles = cloth.get_current_particles()
-    particles_as_py = [[p.x, p.y, p.z, p.w] for p in cur_particles]
-    print(particles_as_py)
-    solver.simulate(1. / 60.)
+cloth.set_gravity(gravity)
 
+
+def gen_simulation():
+    while True:
+        cur_particles = cloth.get_current_particles()
+        particles_as_py = [[p.x, p.y, p.z, p.w] for p in cur_particles]
+        print(particles_as_py)
+        solver.simulate(1. / 60.)
+        yield np.array(particles_as_py)[:, :3]
+
+
+simul_gen = gen_simulation()
+
+
+def viewer_callback(scene):
+    verts = next(simul_gen)
+    render_mesh.vertices = verts
+
+
+v = SceneViewer(trimesh.Scene([render_mesh]), start_loop=False, callback=viewer_callback)
+pyglet.app.run()
+
+print('clearing up!', flush=True)
 solver.remove_cloth(cloth)
 del solver
 del cloth
