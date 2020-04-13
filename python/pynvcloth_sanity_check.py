4adc9421eae772340ca8788318e707da2ab0f422
Daniel Keyes <danieljkeyes@gmail.com>
2020-04-12 20:13:04 -0500

    Wrap some Factory functions, and check they they are callable

--- python/pynvcloth_sanity_check.py
+++ python/pynvcloth_sanity_check.py
@@ -5,3 +5,27 @@ import os
 # directory
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../build/bin/'))
 import pynvcloth
+
+# The NvCloth environment must be setup (and destroyed) explicitly.
+# daniel: This doesn't seem very python-like. Can we do this implicitly?
+pynvcloth.allocate_env()
+
+# Cloth factories can be created via the appropriate constructor.
+null_factory = pynvcloth.Factory()
+print(type(null_factory))
+
+factory = pynvcloth.create_factory_cpu()
+print(type(factory))
+
+# Note: resources need to be explicitly freed. We could also control this with function scoping
+del null_factory
+
+del factory
+
+def scoped_alloc():
+    another = pynvcloth.create_factory_cpu()
+    print(type(another))
+
+scoped_alloc()
+
+pynvcloth.free_env()
--- src/nvcloth_wrapper.cpp
+++ src/nvcloth_wrapper.cpp
@@ -1,10 +1,36 @@
 #include <pybind11/pybind11.h>
 
+#include <NvCloth/Factory.h>
+
 #include "CallbackImplementations.h"
+
+using std::unique_ptr;
+
+namespace py = pybind11;
+
+struct FactoryDeleter {
+	void operator()(nv::cloth::Factory* const factory) const {
+		NvClothDestroyFactory(factory);
+	}
+};
+
+using Factory = unique_ptr<nv::cloth::Factory, FactoryDeleter>;
+// We are using a unique_ptr to enforce a custom deleter, so don't let pybind11
+// try to extract out the raw pointer.
+PYBIND11_MAKE_OPAQUE(Factory);
+
+auto create_factory_cpu()
+{
+	return Factory(NvClothCreateFactoryCPU());
+}
 
 PYBIND11_MODULE(pynvcloth, m) {
 	m.doc() = "A python wrapper around NvCloth";
 
 	m.def("allocate_env", &NvClothEnvironment::AllocateEnv, "Initialize the NvCloth library and register necessary handlers.");
 	m.def("free_env", &NvClothEnvironment::FreeEnv, "De-initialize the NvCloth library.");
+
+	py::class_<Factory>(m, "Factory").def(py::init<>());
+
+	m.def("create_factory_cpu", &create_factory_cpu);
 }
