ee82b04255f08e1f1bf8009c7b7707f0e6b07d5c
Daniel Keyes <danieljkeyes@gmail.com>
2020-04-13 04:08:45 -0500

    add more vertices to the mesh (6 -> 30x30) and add some out-of-plane deformation
    (if all the z coordinates are exactly 0, it stays in a weird planar edge
    case)

--- python/pynvcloth_sanity_check.py
+++ python/pynvcloth_sanity_check.py
@@ -39,26 +39,35 @@ cmd = nvc.ClothMeshDesc()
 cmd.set_to_default()
 print(cmd.is_valid(), flush=True)
 
-vertices = [
-    [0., 0., 0.],
-    [0., 1., 0.],
-    [1., 0., 0.],
-    [1., 1., 0.],
-    [2., 0., 0.],
-    [2., 1., 0.],
-]
-faces = [
-    [0, 3, 1],
-    [0, 2, 3],
-    [2, 5, 3],
-    [2, 4, 5]
-]
+rows = 30
+cols = 30
+dx = 1.0 / rows
+dy = 1.0 / cols
+vertices = []
+faces = []
+inv_masses = []
+for r in range(rows):
+    for c in range(cols):
+        x = c * dx
+        y = -r * dy
+        z = np.sin(np.deg2rad((x + y) * 360 * 5)) * 0.01
+        vertices.append([x, y, z])
+        inv_masses.append(1.0)
+        if r > 0 and c > 0:
+            p00 = (r - 1) * cols + (c - 1)
+            p01 = (r - 1) * cols + (c - 0)
+            p10 = (r - 0) * cols + (c - 1)
+            p11 = (r - 0) * cols + (c - 0)
+            faces.append([p00, p11, p01])
+            faces.append([p00, p10, p11])
+inv_masses[0] = 0.0  # anchor particle 0
+
 render_mesh = trimesh.Trimesh(vertices, faces, process=False)
 
 # Note: these must be declared as named variables so they stay in scope while the mesh is created.
 triangles = nvc.VectorTri([nvc.Triangle(f[0], f[1], f[2]) for f in faces])
 points = nvc.VectorNx3([nvc.Vec3(v[0], v[1], v[2]) for v in vertices])
-inv_masses = nvc.VectorFloat([0.0, 1.0, 1.0, 1.0, 1.0, 1.0])  # anchor particle 0
+inv_masses = nvc.VectorFloat(inv_masses)
 point_masses = nvc.VectorNx4([nvc.Vec4(v, w) for (v, w) in zip(points, inv_masses)])
 
 cmd.triangles = nvc.as_bounded_data(triangles)
@@ -85,8 +94,8 @@ def gen_simulation():
     while True:
         cur_particles = cloth.get_current_particles()
         particles_as_py = [[p.x, p.y, p.z, p.w] for p in cur_particles]
-        print(particles_as_py)
         solver.simulate(1. / 60.)
+        print('updated!')
         yield np.array(particles_as_py)[:, :3]
 
 
